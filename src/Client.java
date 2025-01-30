73f0226c31a1b02f0e8d0243518d153897b27ff6
andrvtl <andreavitale.scuola@gmail.com>
2025-01-30 11:47:29 +0100

    Primo commit client

---
+++ src/Client.java
@@ -0,0 +1,56 @@
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Alessandro Stefanucci
+ */
+public class Client {
+
+    String nome;
+    String colore;
+    Socket socket;
+
+
+    public Client(String nome){
+
+
+    }
+
+    public Client(String nome, String colore){
+
+
+    }
+
+    public void connetti(String nomeServer, int portaServer){
+
+        try {
+            socket = new Socket(nomeServer, portaServer);
+            System.out.println("1) Connessione con il server avvenuta");
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Errore nello stabilimento della connessione con il server");
+        }
+
+    }
+
+
+    public void scrivi(){
+
+
+    }
+
+    public void leggi(){
+
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Alessandro Stefanucci
+ */
+public class Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Client c = new Client("localhost");
+        c.connetti("localhost",2000);
+    }
+
+}
