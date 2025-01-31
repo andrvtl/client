8c8cccf5d51de522d1d471a636f78b751195dd7f
Andrea Vitale <andreavitale.scuola@gmail.com>
2025-01-31 11:40:58 +0100

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,6 @@
 
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -35,7 +36,11 @@ public class Client {
         try {
             socket = new Socket(nomeServer, portaServer);
             System.out.println("1) Connessione con il server avvenuta");
-        } catch (IOException ex) {
+        }
+        catch(ConnectException ex){
+            System.err.println("Errore di connessione, server non in ascolto");
+        }
+        catch (IOException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
             System.err.println("Errore nello stabilimento della connessione con il server");
         }
@@ -45,6 +50,18 @@ public class Client {
 
     public void scrivi(){
 
+
+    }
+
+    public void chiudi(){
+        if(socket!=null) {
+            try {
+                socket.close();
+                System.out.println("5) Chiusura connessione avvenuta con successo");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
     }
 
