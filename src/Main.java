de7541262ab21e26c7da015f4365044f8eb21404
Andrea Vitale <andreavitale.scuola@gmail.com>
2025-02-13 11:39:37 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 /**
  *
  * @author andrvtl
@@ -12,11 +17,19 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+
     public static void main(String[] args) {
+
+        // Inizializzazioni stream e scanner per scrittura e lettura Client
+        InputStream is;
+        Scanner streamIn = null;
+        OutputStream os;
+        PrintWriter streamOut = null;
 
         Client c = new Client("localhost");
         c.connetti("localhost",2000);
         c.chiudi();
+
     }
 
 }
