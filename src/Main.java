2d2a8ea9422e0c6b21e8709fb52e212d02605f14
Andrea Vitale <andreavitale.scuola@gmail.com>
2025-02-14 11:43:36 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.Scanner;
 
 /**
@@ -18,17 +20,42 @@ public class Main {
      * @param args the command line arguments
      */
 
-    public static void main(String[] args) {
+    public static void main(String[] args, Socket socket) {
 
         // Inizializzazioni stream e scanner per scrittura e lettura Client
         InputStream is;
         Scanner streamIn = null;
         OutputStream os;
         PrintWriter streamOut = null;
+        String messaggioIn, messaggioOut;
 
-        Client c = new Client("localhost");
-        c.connetti("localhost",2000);
-        c.chiudi();
+        try {
+            // Inizializza client e connessione
+            Client c = new Client("localhost");
+            c.connetti("localhost",2000);
+
+            // Crea stream 
+            os = socket.getOutputStream();
+            streamOut = new PrintWriter(os);
+            streamOut.flush();
+            
+            is = socket.getInputStream();
+            streamIn = new Scanner(is);
+            
+            messaggioIn = streamIn.nextLine();
+            System.out.println("Messaggio del server: " + messaggioIn);
+            messaggioOut = "Ci sono!";
+            streamOut.println(messaggioOut);
+            streamOut.flush();
+            
+            c.chiudi();
+            
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } 
+        finally {
+            
+        }
 
     }
 
